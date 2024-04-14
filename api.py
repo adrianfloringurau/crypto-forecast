9e82ee76b6ef65c93410119c3e44f68eee2d1569
DeadlyFox <adigurau.deadlyfox@gmail.com>
2024-04-14 08:23:17 +0300

    second commit

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import datetime
 import extractor
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['GET'])
 def get_prediction():
